13adadf22cb89a5a4dd2464fa62155d7fd6eaa45
Julien Chiquet <julien.chiquet@inrae.fr>
2026-01-29 11:15:10 +0100

    added test file for bounded regression

---
+++ tests/testthat/test-breg.R
@@ -0,0 +1,37 @@
+testDataBreg <- readRDS("dataTest-boundedReg.rds")
+
+tol <- 1e-6
+
+test_that("Bounded regression with lambda2 = 0 - test on the documentation example", {
+  
+  res <- bounded.reg(
+    testDataBreg$x,
+    testDataBreg$y,
+    lambda2 = 0
+    )
+  expect_equal(res$coefficients, testDataBreg$breg_lambda2_0$coefficients, tolerance = tol)
+  
+})
+
+test_that("Bounded regression with lambda2 = 5 - test on the documentation example", {
+  
+  res <- bounded.reg(
+    testDataBreg$x,
+    testDataBreg$y,
+    lambda2 = 5
+  )
+  expect_equal(res$coefficients, testDataBreg$breg_lambda2_5$coefficients, tolerance = tol)
+  
+})
+
+test_that("Bounded regression with lambda2 = 10 + S  - test on the documentation example", {
+  
+  res <- bounded.reg(
+    testDataBreg$x,
+    testDataBreg$y,
+    lambda2 = 10, 
+    struct = testDataBreg$S
+  )
+  expect_equal(res$coefficients, testDataBreg$breg_lambda2_10_S$coefficients, tolerance = tol)
+  
+})
