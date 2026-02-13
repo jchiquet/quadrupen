e56d6f829a7c7a0c42a3dc1e73a26da712e91a7e
Julien Chiquet <julien.chiquet@inrae.fr>
2026-02-13 09:24:30 +0100

    updating demo so that they partially work

--- demo/quad_lasso.R
+++ demo/quad_lasso.R
@@ -8,7 +8,7 @@ eps <- 0.25 # correlation between relevant and irrelevant variable
 S11 <- toeplitz(cor^(0:(80-1))) ## Toeplitz correlation for irrelevant variable
 S22 <- matrix(cor,10,10) ## bloc correlation bewteen relevant variables
 diag(S22) <- 1
-Sigma <- bdiag(S11,S22,S22) + eps
+Sigma <- Matrix::bdiag(S11,S22,S22) + eps
 
 ## Build a vector of label for true nonzeros
 labels <- rep("irrelevant", length(beta))
@@ -16,25 +16,26 @@ labels[beta != 0] <- c("relevant")
 labels <- factor(labels, ordered=TRUE, levels=c("relevant","irrelevant"))
 
 ## uncomment if you want insights on the correlation structure
-## image(Sigma)
+## Matrix::image(Sigma)
 x <- as.matrix(matrix(rnorm(100*n),n,100) %*% chol(Sigma))
 y <- mu + x %*% beta + rnorm(n, 0, sigma)
 
-## Test simple and double cross-validation
-cv.simple <- crossval(x, y, "lasso")
-plot(cv.simple)
+## Fit the lasso
 fit <- lasso(x,y)
-
 ## plot the solution path
 plot(fit)
 ## a quick summary of the fit
 print(fit)
 ## AIC and BIC
-criteria(fit)
+plot(fit, "criteria")
+
+## Test simple and double cross-validation
+cv.simple <- cross_validate(fit)
+plot(cv.simple)
 
 ## Call to stability selection function, 200 subsampling
-stab <- stability(x,y, "lasso", subsamples=200, min.ratio=1e-2)
-## plot the stability path
-plot(stab, labels=labels, nvar=20)
+stab <- stability(fit, n_subsamples = 200, cores = 10)
 ## a quick summary of the fit
 print(stab)
+## plot the stability path
+stab$plot()
