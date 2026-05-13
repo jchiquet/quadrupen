51157df3517ae9a88d5c25e10f94b4276de7e730
Julien Chiquet <julien.chiquet@inrae.fr>
2026-05-13 17:41:05 +0200

    adding test ofr SCAD and MCP (which fail at the moment)

---
+++ tests/testthat/test-mcp.R
@@ -0,0 +1,73 @@
+context("Consistency of the Lasso solution paths (package 'lars' and 'glmnet')")
+
+testDataEnet <- readRDS("dataTest-Enet.rds")
+
+tol <- 1e-2
+
+get_mcp <- function(x, y, gamma = 3,  method = "quadra") {
+  
+  require(ncvreg)
+  
+  mcp_ncvreg <- ncvreg(x, y, gamma = gamma, family="gaussian", penalty = "MCP")
+  coef   <- mcp_ncvreg$beta[-1,]
+  lambda <- mcp_ncvreg$lambda
+  n <- nrow(x)
+  
+  mcp_quadru <- mcp(x, y, lambda1 = lambda * sqrt(n), eta = gamma,
+                           control = list(method = method))
+  coef_quad <- as.matrix(mcp_quadru$coefficients)
+  
+  if (plot) {
+    matplot(lambda, t(coef), type = "l", log = "x")
+    matplot(lambda * sqrt(n), t(coef_quad), type = "l", log = "x")
+  }
+  
+  res <- list(quadru=coef_quad, ncvreg=coef)
+  res
+}
+
+test_that("MCP with Newton solver", {
+  
+  ## PROSTATE DATA SET
+  x <- testDataEnet$x_prostate
+  y <- testDataEnet$y_prostate
+  
+  ## Run the tests...
+  out <- get_mcp(x,y)
+  expect_equal(out$quadru, out$ncvreg,
+               check.attributes = FALSE, tolerance = tol)
+  
+  ## RANDOM DATA
+  x <- testDataEnet$x_sim
+  y <- testDataEnet$y_sim
+  
+  ## Run the tests...
+  out <- get_mcp(x,y)
+  expect_equal(out$quadru, out$ncvreg,
+               check.attributes = FALSE, tolerance = tol)
+  
+})
+
+
+test_that("MCP with FISTA solver", {
+  
+  ## PROSTATE DATA SET
+  x <- testDataEnet$x_prostate
+  y <- testDataEnet$y_prostate
+  
+  ## Run the tests...
+  out <- get_mcp(x,y, "fista")
+  expect_equal(out$quadru, out$ncvreg,
+               check.attributes = FALSE, tolerance = tol)
+  
+  ## RANDOM DATA
+  x <- testDataEnet$x_sim
+  y <- testDataEnet$y_sim
+  
+  ## Run the tests...
+  out <- get_mcp(x,y, "fista")
+  expect_equal(out$quadru, out$ncvreg,
+               check.attributes = FALSE, tolerance = tol)
+  
+})
+```
